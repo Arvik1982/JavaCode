68cb8af1d58b02822fcdeedfe82013cea150a7e1
Арсений <@email>
2026-04-08 15:57:51 +0300

    improve: calculator tip

--- CalculatorTip.java
+++ CalculatorTip.java
@@ -1,19 +1,34 @@
+import java.util.Scanner;
+
 public class CalculatorTip {
-    static double billAmount = 1000;
-    static double tipPercentage = 10;
-    static int numberOfPeople = 4;
+    // static double billAmount = 1000;
+    // static double tipPercentage = 10;
+    // static int numberOfPeople = 4;
 
     public static void main(String[] args) {
-        double tipAmount = billAmount * tipPercentage / 100;
-        double totalAmount = billAmount + tipAmount;
-        double perPersonAmount = totalAmount / numberOfPeople;
 
-        System.out.printf("Сумма счёта: %.2f%n", billAmount);
-        System.out.printf("Процент чаевых: %.2f%%%n", tipPercentage);
-        System.out.printf("Сколько человек участвовало в обеде: %d%n", numberOfPeople);
-        System.out.printf("Общий счёт: %.2f%n", totalAmount);
-        System.out.printf("Процент чаевых: %.2f%%%n", tipAmount);
-        System.out.printf("С каждого: %.2f%n", perPersonAmount);
+        try (Scanner scanner = new Scanner(System.in)) {
+
+            System.out.printf("Введите сумму счёта: ");
+            double billAmount = Double.parseDouble(scanner.nextLine().replace(",", "."));
+
+            System.out.printf("Введите процент чаевых: ");
+            double tipPercentage = Double.parseDouble(scanner.nextLine().replace(",", "."));
+
+            System.out.printf("Введите количество участников: ");
+            int numberOfPeople = Integer.parseInt(scanner.nextLine().replace(",", "."));
+
+            double tipAmount = billAmount * tipPercentage / 100;
+            double totalAmount = billAmount + tipAmount;
+            double perPersonAmount = totalAmount / numberOfPeople;
+
+            System.out.printf("Сумма счёта: %.2f%n", billAmount);
+            System.out.printf("Процент чаевых: %.2f%%%n", tipPercentage);
+            System.out.printf("Сколько человек участвовало в обеде: %d%n", numberOfPeople);
+            System.out.printf("Общий счёт: %.2f%n", totalAmount);
+            // System.out.printf("Процент чаевых: %.2f%%%n", tipAmount);
+            System.out.printf("С каждого: %.2f%n", perPersonAmount);
+        }
     }
     // @formatter:off
     /**
